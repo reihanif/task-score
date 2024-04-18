1dace0f45a980c558fdc915996b446d297209a53
Reihan Faizal Hanif <mk.reihan.hanif@pertamina.com>
2024-04-18 10:53:35 +0700

    (features/create-assignment): create Assignment model and it's relationship

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -2,10 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Assignment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'due' => 'datetime',
+        'resolved_at' => 'datetime',
+        'closed_at' => 'datetime'
+    ];
+
+    /**
+     * Get the parent of the assignments.
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Assignment::class, 'parent_id');
+    }
+
+    /**
+     * Get the assignee of the assignments.
+     */
+    public function asignee(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    /**
+     * Get the taskmaster of the assignments.
+     */
+    public function taskmaster(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'taskmaster_id');
+    }
 }
