080b98554d08ef13f9b9d099ea8d1b9d8c0142d7
Reihan Faizal Hanif <mk.reihan.hanif@pertamina.com>
2024-04-18 10:27:04 +0700

    (features/create-assignment): create migration structure

--- database/migrations/2024_04_18_091943_create_assignments_table.php
+++ database/migrations/2024_04_18_091943_create_assignments_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('assignments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('parent_id')->nullable()->references('id')->on('assignments')->onDelete('cascade');
+            $table->foreignUuid('taskmaster_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignUuid('assigned_to')->references('id')->on('users')->onDelete('cascade');
+            $table->string('type');
+            $table->string('subject');
+            $table->text('description');
+            $table->datetime('due');
+            $table->datetime('resolved_at');
+            $table->enum('status', array('open','closed'))->default('open');
+            $table->datetime('closed_at');
             $table->timestamps();
         });
     }
