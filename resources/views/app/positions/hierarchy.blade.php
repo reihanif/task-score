ea5d6e5919d6f250d161a93c3f39b4f9a876a5bd
Reihan Faizal Hanif <mk.reihan.hanif@pertamina.com>
2024-05-07 08:41:45 +0700

    refactor hierarchy

--- resources/views/app/positions/hierarchy.blade.php
+++ resources/views/app/positions/hierarchy.blade.php
@@ -10,56 +10,13 @@
     </div>
     <div class="tree text-gray-900 dark:text-white">
         <ul>
-            @foreach ($positions as $key => $position)
+            @foreach ($positions as $position)
                 @if ($position->level == 0)
                     <li>
                         <a href="#">
                             {{ $position->name }}
                         </a>
-                        @if (!$position->direct_subordinates->isEmpty())
-                            <ul>
-                                @foreach ($position->direct_subordinates as $subordinate1)
-                                    <li>
-                                        <a href="#">
-                                            {{ $subordinate1->name }}
-                                        </a>
-                                        @if (!$subordinate1->direct_subordinates->isEmpty())
-                                            <ul>
-                                                @foreach ($subordinate1->direct_subordinates as $subordinate2)
-                                                    <li>
-                                                        <a href="#">
-                                                            {{ $subordinate2->name }}
-                                                        </a>
-                                                        @if (!$subordinate2->direct_subordinates->isEmpty())
-                                                            <ul>
-                                                                @foreach ($subordinate2->direct_subordinates as $subordinate3)
-                                                                    <li>
-                                                                        <a href="#">
-                                                                            {{ $subordinate3->name }}
-                                                                        </a>
-                                                                        @if (!$subordinate3->direct_subordinates->isEmpty())
-                                                                            <ul>
-                                                                                @foreach ($subordinate3->direct_subordinates as $subordinate4)
-                                                                                    <li>
-                                                                                        <a href="#">
-                                                                                            {{ $subordinate4->name }}
-                                                                                        </a>
-                                                                                    </li>
-                                                                                @endforeach
-                                                                            </ul>
-                                                                        @endif
-                                                                    </li>
-                                                                @endforeach
-                                                            </ul>
-                                                        @endif
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
+                        @include('app.positions.subhierarchy', ['subordinates' => $position->direct_subordinates])
                     </li>
                 @endif
             @endforeach
