a95bbef83441c5c22f100432343c212ea33a8b15
reihanif <161105975+reihanif@users.noreply.github.com>
2024-08-12 07:50:43 +0700

    fix/submission (#59)
    * bugfix/create-user-page
    
    * change assignment detail for assignee to nullable
    
    * remove close-reopen assignment button
    
    * fix/error exception die remove
    
    * Fix: Improve mobile responsiveness on the dashboard page
    
    * refactor/create-assignment-page
    
    * bugfix/create-assignment-page
    
    * Fix: Improve mobile responsiveness on the my assignment page
    
    * fix: padding bug in tab component
    
    * Refactor/texteditor (#53)
    
    * build/tiptap link extension
    
    * refactor texteditor to add link features
    
    * update date data ordering
    
    * Fix/subordinate assignments (#57)
    
    * update date data ordering
    
    * add submission status & total waiting approval in sidebar
    
    * fix submission detail to break-words
    
    ---------
    
    Co-authored-by: Fadhlan54 <muhammadfadhlan0011@gmail.com>
    Co-authored-by: Muhammad Fadhlan Aziz <102808965+Fadhlan54@users.noreply.github.com>

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -187,6 +187,48 @@ class Task extends Model
     }
 
     /**
+     * Create score attribute.
+     */
+    public function getScoreAttribute()
+    {
+        return $this->score();
+    }
+
+    /**
+     * Create submission status badge attribute.
+     */
+    public function getSubmissionStatusBadgeAttribute()
+    {
+        if ($this->latestSubmission?->isWaitingApproval()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-yellow-800 bg-yellow-200 rounded-full">Waiting for approval</span>';
+        } elseif ($this->latestSubmission?->isApproved()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-green-800 bg-green-200 rounded-full">Resolved</span>';
+        } elseif ($this->latestSubmission?->isNotApproved()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-red-800 bg-red-200 rounded-full">Rejected</span>';
+        } else {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-gray-800 dark:text-gray-400 rounded-full">-</span>';
+        }
+        return Str::of($status)->toHtmlString;
+    }
+
+    /**
+     * Create time extension status badge attribute.
+     */
+    public function getTimeExtensionStatusBadgeAttribute()
+    {
+        if ($this->latestTimeExtension?->isWaitingApproval()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-yellow-800 bg-yellow-200 rounded-full">Waiting for approval</span>';
+        } elseif ($this->latestTimeExtension?->isApproved()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-green-800 bg-green-200 rounded-full">Approved</span>';
+        } elseif ($this->latestTimeExtension?->isNotApproved()) {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-red-800 bg-red-200 rounded-full">Rejected</span>';
+        } else {
+            $status = '<span class="inline-flex items-center justify-center px-1.5 w-fit h-4 text-xs font-semibold text-gray-800 dark:text-gray-400 rounded-full">-</span>';
+        }
+        return Str::of($status)->toHtmlString;
+    }
+
+    /**
      * Calculate task score.
      */
     public function score()
