22598f687ed02312c1603925ce350a77a3bf2ce1
reihanif <reihanfaizalhanif@gmail.com>
2024-08-14 08:30:25 +0700

    fix/permissions table structure and model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -14,7 +14,25 @@ class Permission extends Model
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'user_id'
-    ];
+    protected $fillable = ['name'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function assignToUser($user)
+    {
+        if (is_string($user)) {
+            $user = User::where('id', $user)->firstOrFail();
+        }
+        $this->users()->attach($user);
+    }
+
+    public function scopeWithUser($query, $userId)
+    {
+        return $query->whereHas('users', function ($q) use ($userId) {
+            $q->where('user_id', $userId);
+        });
+    }
 }
