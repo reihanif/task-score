5d41838433d2f1b2515e1c95d1741282d3613c51
reihanif <reihanfaizalhanif@gmail.com>
2024-08-27 14:53:06 +0700

    bugfix/edit due form

--- resources/views/app/taskscore/assignments/modals/approve-time-extension.blade.php
+++ resources/views/app/taskscore/assignments/modals/approve-time-extension.blade.php
@@ -21,14 +21,14 @@
                             <div class="interval flex items-center ps-3">
                                 <input
                                     class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                                    id="select-interval"
+                                    id="select-interval{{ $time_extension->id }}"
                                     type="radio"
                                     value="interval-time"
-                                    name="due"
+                                    name="due{{ $time_extension->id }}"
                                     x-model="selectedOption"
                                     required>
                                 <label class="ms-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                    for="select-interval">
+                                    for="select-interval{{ $time_extension->id }}">
                                     Select by interval
                                 </label>
                             </div>
@@ -36,14 +36,14 @@
                             <div class="exact-time flex items-center ps-3">
                                 <input
                                     class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                                    id="select-exact-time"
+                                    id="select-exact-time{{ $time_extension->id }}"
                                     type="radio"
                                     value="exact-time"
-                                    name="due"
+                                    name="due{{ $time_extension->id }}"
                                     x-model="selectedOption"
                                     required>
                                 <label class="ms-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                    for="select-exact-time">
+                                    for="select-exact-time{{ $time_extension->id }}">
                                     Select exact time
                                 </label>
                             </div>
