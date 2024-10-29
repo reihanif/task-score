69d7f53756415bfdedac780cfb13ef206d030a1f
reihanif <reihanfaizalhanif@gmail.com>
2024-10-29 10:36:02 +0700

    fix/remindme button

--- resources/views/app/taskscore/index.blade.php
+++ resources/views/app/taskscore/index.blade.php
@@ -18,7 +18,7 @@
                             <div class="text-sm font-semibold text-gray-700 dark:text-white">
                                 Welcome, {{ auth()->user()->name }}
                             </div>
-                            <a href="{{ route('redirect') }}" class="inline-flex rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-500 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+                            <a href="{{ route('remindme') }}" class="inline-flex rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-500 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
                             target="_blank">
                                 Remindme
                             </a>
